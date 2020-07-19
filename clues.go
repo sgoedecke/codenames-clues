d3928926b962df980f45ffa8d6a286ba4a35efa1
Sean Goedecke <sgoedecke@zendesk.com>
2020-07-19 14:12:19 +1000

    more words

--- clues.go
+++ clues.go
@@ -19,7 +19,7 @@ func buildIndex(filePaths []string) map[string][]string {
 
 	fmt.Println("Building index...")
 
-	commonWords := []string{"by", "of", "for", "in", "and", "is", "from", "was", "the", "a", "an", "into", "as", "but", "with", "to", "who", "which", "out", "also", "each", "where", "than", "has", "that", "not", "on", "so", "no", "its", "would", "may", "began", "became", "gave", "till", "other", "his", "one", "two", "upon", "during", "it", "then", "after", "many", "de", "et", "came", "be", "there", "are", "all", "their", "went", "were", "some", "any", "very", "they", "when", "this", "again", "himself", "him", "her", "herself", "or", "defn", "over", "among", "had", "q", "both"}
+	commonWords := []string{"by", "of", "for", "in", "and", "is", "from", "was", "the", "a", "an", "into", "as", "but", "with", "to", "who", "which", "out", "also", "each", "where", "than", "has", "that", "not", "on", "so", "no", "its", "would", "may", "began", "became", "gave", "till", "other", "his", "one", "two", "upon", "during", "it", "then", "after", "many", "de", "et", "came", "be", "there", "are", "all", "their", "went", "were", "some", "any", "very", "they", "when", "this", "again", "himself", "him", "her", "herself", "or", "defn", "over", "among", "had", "q", "both", "if", "our", "do", "have", "can", "your", "while", "now", "did", "we", "get", "now", "give", "do", "up", "such", "those", "etym", "akin", "obs", "vb", "these", "been"}
 
 	blocks := strings.Split(data, "\n")
 	for _, block := range blocks {
